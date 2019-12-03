d8c13dacf548071da66e7a7ce6490a824d1aa05d
doberan <doberankei@gmail.com>
2019-12-03 19:14:13 +0900

    add component

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,4 @@
-use yew::prelude::*;
-
-struct Model {
-    value: i64,
-}
-
-enum Msg {
-    DoIt,
-}
-
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Self {
-            value: 0,
-        }
-    }
-
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::DoIt => self.value = self.value + 1
-        }
-        true
-    }
-
-    fn view(&self) -> Html<Self> {
-        html! {
-            <div>
-               <button onclick=|_| Msg::DoIt>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
-    }
-}
 fn main() {
-    yew::initialize();
-    App::<Model>::new().mount_to_body();
+    yew::App::<yew_sample_rs::Model>::new().mount_to_body();
     yew::run_loop();
 }
