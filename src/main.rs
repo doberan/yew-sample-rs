5c54dd5cdaef0d59ee74ff6a822aaefd4dec669e
suzuki_masaya <suzuki_masaya@kthree.co.jp>
2020-02-26 18:32:10 +0900

    add yew counter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+extern crate yew_sample_rs as ysr;
+
 fn main() {
-    yew::App::<yew_sample_rs::Model>::new().mount_to_body();
-    yew::run_loop();
+    // yew::App::<yew_sample_rs::Model>::new().mount_to_body();
+    // yew::run_loop();
+    // println!("hello world");
+    yew::start_app::<ysr::Model>();
 }
